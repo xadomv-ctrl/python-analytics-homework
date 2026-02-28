ad48f345c6e7f1b47f16952a9867be482454fe7c
xadomv-ctrl <xadomv@gmail.com>
2026-02-28 22:16:40 +0200

    Revert "Add comment about average sales"

--- analysis.py
+++ analysis.py
@@ -1,12 +1,9 @@
 import pandas as pd
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 df = pd.DataFrame(data)
-print("Продажі по містах (тимчасова версія):")
+print("Продажі по містах:")
 print(df)
-average_sales = df["sales"].mean()
-print("Середнє значення:", average_sales)
-print("Це середній рівень продажів по трьох містах")
-
+print("Середнє значення:", df["sales"].mean())
 
 
 
